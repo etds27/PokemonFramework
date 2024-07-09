c7e298d06fd4999c254605ea98c815740c0587b0
Ethan Schwarz <etds27@gmail.com>
2024-07-08 18:26:35 -0700

    Added emulator bridge

---
+++ src/EmulatorBridge/EmulatorBridge.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PokemonFramework.EmulatorBridge
+{
+    public static class EmulatorBridge
+    {
+        public static IEmulatorInterface GetEmulator(string emulatorName)
+        {
+            return emulatorName switch
+            {
+                "BizHawk" => new BizHawkEmulatorBridge(),
+                _ => new BizHawkEmulatorBridge(),
+            };
+        }
+    }
+}
